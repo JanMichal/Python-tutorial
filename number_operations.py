7a75b2cf72397dafc07099967e8e51a0ff780fe2
Jan Korona <mimulec@gmail.com>
2017-12-21 10:07:56 +0100

    Kilka plików .py

---
+++ number_operations.py
@@ -0,0 +1,14 @@
+# line of comments
+exponents = 10 ** 20
+print (exponents)
+
+
+"""
+this multi-line comment
+modulo - It returns the reminder
+"""
+
+remainder = 11 % 3
+print (remainder)
+
+# '%' -> reszta z dzielenia
